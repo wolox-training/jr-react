663682453e77cb08a85dc59f483ecf9d41eeb589
pipe <felipe.rivera@wolox.co>
2021-01-12 17:02:10 -0500

    add file paths.ts

--- src/app/components/Router/index.tsx
+++ src/app/components/Router/index.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from '~screens/Login';
 import Register from '~screens/Register';
+import { PATHS } from '~utils/paths';
 
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/sign_up" component={Register} />
+        <Route exact path={PATHS.login} component={Login} />
+        <Route exact path={PATHS.signUp} component={Register} />
       </Switch>
     </BrowserRouter>
   );
